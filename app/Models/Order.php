8ec0e5e0fe45ac0dc824c6c4254ee2cc101033cc
Myo Zin Kyaw <mzkyaw@ahtaroo.com>
2026-03-03 00:37:21 +0630

    day 5 order in wip

---
+++ app/Models/Order.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+class Order extends BaseModel
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'status',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            //
+        ];
+    }
+}
